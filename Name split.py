02eedff86f26489f46f26bc8810741e3b85b0078
Ragib Hasan <60899437+Raag316@users.noreply.github.com>
2020-10-21 22:48:13 +0100

    Update Name split.py

--- Name split.py
+++ Name split.py
@@ -8,10 +8,10 @@
 # Copyright:   (c) ragib 2020
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-s="My name is Ragib. I am a data science student at Cradiff University"
+s="My name is Ragib and I am a data science student at Cradiff University"
 s1=s.split()
 name= "Ragib"
 for i in s1:
+    #print(i)
     if i==name:
-        print("Name in string")
-    continue
+        print(name)
